3310a72384c53495ab5c48fe3aec15124cb19309
unknown <thabisorankhala@gmail.com>
2024-04-14 23:54:17 +0200

    Created a landing page.

--- front/views.py
+++ front/views.py
@@ -3,3 +3,12 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
 	return render(request, "front/index.html")
+
+def about(request):
+	pass
+
+def login_view(request):
+	pass
+
+def logout_view(request):
+	pass
